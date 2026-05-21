3f3142a75e0c5fa9710065f7c2d0984a4288d21d
kenkanekiken <kennethlim619@hotmail.com>
2026-05-21 20:06:12 +0800

    Remove unused code

--- python_backend/app.py
+++ python_backend/app.py
@@ -1,44 +1,3 @@
-# from flask import Flask, request, jsonify
-# from ultralytics import YOLO
-# import cv2
-# import numpy as np
-
-# app = Flask(__name__)
-
-# model = YOLO("models/my_model.pt")
-
-# @app.route("/detect", methods=["POST"])
-# def detect():
-
-#     file = request.files["image"]
-
-#     image_bytes = file.read()
-
-#     npimg = np.frombuffer(image_bytes, np.uint8)
-
-#     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-
-#     results = model(img)
-
-#     detections = []
-
-#     for r in results:
-#         for box in r.boxes:
-
-#             cls = int(box.cls[0])
-#             conf = float(box.conf[0])
-
-#             detections.append({
-#                 "class": model.names[cls],
-#                 "confidence": conf
-#             })
-
-#     return jsonify(detections)
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
-
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -47,8 +6,7 @@ from ultralytics import YOLO
 app = Flask(__name__)
 CORS(app)
 
-# model = YOLO("models/my_model.pt")
-model = YOLO("models/best.pt")
+model = YOLO("models/my_model.pt")
 
 @app.route("/")
 def home():
@@ -61,16 +19,8 @@ def detect():
 
     file = request.files["image"]
 
-    # img = Image.open(file.stream)
-    # results = model(img)
-    
-    img = Image.open(file.stream).convert("RGB")
-    results = model.predict(
-      img,
-      imgsz=320,
-      device="cpu",
-      verbose=False,
-    )
+    img = Image.open(file.stream)
+    results = model(img)
 
     detections = []
 
